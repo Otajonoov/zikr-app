264244b8d9cc654f17aaa3ecb37a610df47f7808
usmanov0 <azikusmanov882@gmail.com>
2024-04-18 16:18:51 +0500

    implements :Create user if not exist, check password, getByUsername

--- internal/zikr/usecase/auth.go
+++ internal/zikr/usecase/auth.go
@@ -2,6 +2,8 @@ package usecase
 
 import (
 	"context"
+	"errors"
+	"strings"
 	"zikr-app/internal/zikr/domain"
 )
 
@@ -17,32 +19,43 @@ func NewAuthUsecase(repo domain.AuthRepository) *authUsecase {
 
 func (a *authUsecase) CreateUser(ctx context.Context, user *domain.User) error {
 
-	//u, err := a.repo.GetUser(ctx, user.PhoneNumber, user.UniqeUsername)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//if u.PhoneNumber == user.PhoneNumber {
-	//	return errors.New("user is already registered")
-	//}
-
-	if err := a.repo.CreateUser(ctx, user); err != nil {
+	exists, err := a.repo.UserExists(ctx, user.UniqeUsername)
+	if err != nil {
 		return err
 	}
 
-	return nil
-
+	if exists {
+		return errors.New("user already exists")
+	} else {
+		err = a.repo.CreateUser(ctx, user)
+		if err != nil {
+			return errors.New("failed to create user")
+		}
+		return nil
+	}
 }
 
-func (a *authUsecase) CheckUser(ctx context.Context, user *domain.User) bool {
+func (a *authUsecase) CheckUser(ctx context.Context, userName, password string) (bool, error) {
 
-	userData, err := a.repo.GetUser(ctx, user.PhoneNumber)
+	userData, err := a.repo.GetUser(ctx, userName)
 	if err != nil {
-		return false
+		return false, errors.New("failed to get user")
 	}
-	if userData.Password != user.Password {
-		return false
+	if userData.Password != password {
+		return false, errors.New("wrong password")
 	}
 
-	return true
+	return true, nil
+}
+
+func (a *authUsecase) GetByUserName(ctx context.Context, userName string) (*domain.User, error) {
+	if strings.TrimSpace(userName) == "" {
+		return nil, errors.New("empty username")
+	}
+
+	userData, err := a.repo.FindOneByUsername(ctx, userName)
+	if err != nil {
+		return nil, errors.New("failed to get user")
+	}
+	return userData, nil
 }
