212ec73d27de3c315e8f8c5131d3aeed6c1a22de
usmanov0 <azikusmanov882@gmail.com>
2024-04-19 01:25:51 +0500

    relationship to user and added :favorite field

--- migrations/000001_zikr.up.sql
+++ migrations/000001_zikr.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS "zikr" (
     "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES users(id),
     "arabic" TEXT NOT NULL,
     "uzbek" TEXT NOT NULL,
     "pronounce" TEXT NOT NULL,
+    "is_favorite" BOOLEAN DEFAULT FALSE,
     "created_at" TIMESTAMP(0) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(0) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
