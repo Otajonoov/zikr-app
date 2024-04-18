863a301f46acda3d19b30309e515b8a7398e2491
usmanov0 <azikusmanov882@gmail.com>
2024-04-18 15:13:20 +0500

    changed unique username :to Unique column

--- migrations/000002_user.up.sql
+++ migrations/000002_user.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS "users" (
     "id" SERIAL PRIMARY KEY,
     "fio" VARCHAR(255),
     "phone" VARCHAR(255) NOT NULL,
-    "uniqe_username"  VARCHAR(255) NOT NULL,
+    "uniqe_username"  VARCHAR(255) UNIQUE NOT NULL,
     "password" VARCHAR(255) NOT NULL
 );
