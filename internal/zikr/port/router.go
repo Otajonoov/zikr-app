cb98ff67d37a34b01ba6a7c0c3e09b388707c278
usmanov0 <azikusmanov882@gmail.com>
2024-04-29 16:12:10 +0500

    router for all endpoints

--- internal/zikr/port/router.go
+++ internal/zikr/port/router.go
@@ -5,12 +5,12 @@ import (
 	chimiddleware "github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5/pgxpool"
 	httpSwagger "github.com/swaggo/http-swagger"
-	"zikr-app/internal/pkg/jwt"
 	"zikr-app/internal/zikr/adapter"
 	"zikr-app/internal/zikr/domain"
 	handler "zikr-app/internal/zikr/port/http"
 	_ "zikr-app/internal/zikr/port/http/docs"
 	"zikr-app/internal/zikr/usecase"
+	"zikr-app/pkg/jwt"
 )
 
 type RouterOption struct {
@@ -37,19 +37,6 @@ func New(option RouterOption) *chi.Mux {
 
 	factory := domain.NewZikrFactory()
 
-	// Zikr
-	zikrRepo := adapter.NewZikrRepo(option.DB, factory)
-	zikrUsecase := usecase.NewZikrUsecase(zikrRepo, factory)
-	zikrHandler := handler.NewZikrHandler(zikrUsecase)
-
-	// Routers
-	router.Route("/zikr", func(r chi.Router) {
-		r.Post("/create", zikrHandler.Create())
-		r.Get("/get", zikrHandler.Get)
-		r.Get("/get-all", zikrHandler.Create())
-		r.Put("/update", zikrHandler.Create())
-	})
-
 	// Auth
 	authRepo := adapter.NewAuthRepo(option.DB)
 	authUsecase := usecase.NewAuthUsecase(authRepo)
@@ -60,6 +47,36 @@ func New(option RouterOption) *chi.Mux {
 		r.Post("/sign-up-user", authHandler.SignUp)
 		r.Post("/sign-in-user", authHandler.SignIn)
 		r.With(jwt.AuthMiddleWare).Get("/get-user/{username}", authHandler.GetUserByUserName)
+	})
+
+	// Zikr
+	zikrRepo := adapter.NewZikrRepo(option.DB, factory)
+	zikrUsecase := usecase.NewZikrUsecase(zikrRepo, factory)
+	zikrHandler := handler.NewZikrHandler(zikrUsecase)
+
+	// Routers
+	router.Route("/zikr", func(r chi.Router) {
+		r.Post("/create", zikrHandler.Create())
+		r.Get("/get", zikrHandler.Get)
+		r.Get("/get-all", zikrHandler.GetAll)
+		r.Get("/get-favorites", zikrHandler.GetAllFavorites)
+		r.Patch("/favorite", zikrHandler.Favorites)
+		r.Patch("/unfavorite", zikrHandler.UnFavorites)
+		r.Put("/update", zikrHandler.Update)
+		r.Delete("/delete", zikrHandler.Delete)
+	})
+
+	// ZikrCount
+	zikrCountRepo := adapter.NewZikrCountRepo(option.DB)
+	zikrCountUseCase := usecase.NewZikrCountUsecase(zikrCountRepo)
+	zikrCountHandler := handler.NewZikrCountHandler(zikrCountUseCase)
+
+	// Routers
+	router.Route("/zikr-count", func(r chi.Router) {
+		r.Post("/add-count", zikrCountHandler.CreateCount)
+		r.Get("/list-user-counts", zikrCountHandler.ListCount)
+		r.Patch("/increment", zikrCountHandler.PatchUserCount)
+		r.Patch("/reset", zikrCountHandler.Delete)
 	})
 
 	// Swagger integration
